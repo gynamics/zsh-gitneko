104271fdd449b637076241f768824fdcdbd18618
gynamics <dybfysiat@163.com>
2023-09-10 02:58:52 +0800

    Update gitneko.zsh
    Reconstructed and reformatted code.
    Fix bugs of not update git status timely.
    Fix bugs of python venv prompt compatiblity.
    Add a status check table in help information.
    
    Signed-off-by: gynamics <dybfysiat@163.com>

--- gitneko.zsh
+++ gitneko.zsh
@@ -22,147 +22,168 @@ NEKOLOR_Y='%B%F{yellow}'
 NEKOLOR_W='%B%F{white}'
 
 # get git status and save it to NEKOPS
-gitneko-get() {
-  local refname=$(< ${NEKOPS_HEAD}/.git/HEAD)
-  if [[ $refname =~ "ref: refs/heads/.*" ]]; then
-    refname=${refname#ref: refs/heads/}
-  else
-    refname=${refname:0:6}
-  fi
-  if [[ -n $(git remote) ]]; then
-    NEKOPS_BRCH="${NEKOLOR_B}${refname} ${NEKOLOR_C}ᛘ"
-  else
-    NEKOPS_BRCH="${NEKOLOR_Y}${refname} ${NEKOLOR_W}ᛘ"
-  fi
-  NEKOPS_ARG1="${NEKOLOR_M}?"
-  NEKOPS_ARG2=""
-  NEKOPS_ARG3=""
-  if [[ $(pwd) =~ "\.git" ]]; then
-    return # do not run git status in .git directory
-  fi
-  local git_status=$(git --no-optional-locks status --porcelain=v1 .)
-  if [[ $git_status =~ [ADU][ADU][\ ] ]]; then
-    # Updated
-    NEKOPS_ARG1="${NEKOLOR_C}·%"
-  elif [[ $git_status =~ [DMTARC][\ ][\ ] ]]; then
-    # Staged
-    NEKOPS_ARG1="${NEKOLOR_G}6"
-  elif [[ $git_status =~ [\ MTARC][\ AMTD][\ ] ]]; then
-    # Unmerged
-    NEKOPS_ARG1="${NEKOLOR_C}0"
-  elif [[ $git_status =~ [!][!][\ ] ]]; then
-    # Ignored
-    NEKOPS_ARG1="${NEKOLOR_B}-"
-  elif [[ $git_status =~ [X][\ ][\ ] ]]; then
-    # Error
-    NEKOPS_ARG1="${NEKOLOR_R}e"
-  else
-    # Committed
-    NEKOPS_ARG1="${NEKOLOR_W}>"
-    NEKOPS_ARG2="${NEKOLOR_W}<"
-  fi
-  
-  if [[ $git_status =~ [\?][\?][\ ] ]]; then
-    # Untracked
-    NEKOPS_ARG2="${NEKOLOR_M}*"
-  fi
-  
-  local stashcnt=$(git stash list|wc -l)
-  if [ $stashcnt -gt 0 ]; then
-    # Stashed
-    NEKOPS_ARG3="${NEKOPS_ARG3} ${NEKOLOR_Y}≅"
-  fi
-  if [ -d ${NEKOPS_HEAD}/.git/rebase-apply ]; then
-    # In Rebase-Apply State
-    NEKOPS_ARG3="${NEKOPS_ARG3} ${NEKOLOR_R}a"
-  fi
+gitneko-get-status() {
+# do not run 'git status' under .git directory
+if [[ $NEKOPS_PATH =~ "\.git" ]]; then
+  return
+fi
+# reset nekops args
+NEKOPS_ARG1="${NEKOLOR_M}?"
+NEKOPS_ARG2=""
+NEKOPS_ARG3=""
+# get status and set nekops args
+local git_status=$(git --no-optional-locks status --porcelain=v1 .)
+if [[ $git_status =~ [ADU][ADU][\ ] ]]; then
+  # Updated
+  NEKOPS_ARG1="${NEKOLOR_Y}*"
+elif [[ $git_status =~ [DMTARC][\ ][\ ] ]]; then
+  # Staged
+  NEKOPS_ARG1="${NEKOLOR_G}6"
+elif [[ $git_status =~ [\ MTARC][\ AMTD][\ ] ]]; then
+  # Unmerged
+  NEKOPS_ARG1="${NEKOLOR_C}0"
+elif [[ $git_status =~ [!][!][\ ] ]]; then
+  # Ignored
+  NEKOPS_ARG1="${NEKOLOR_M}-"
+elif [[ $git_status =~ [X][\ ][\ ] ]]; then
+  # Error
+  NEKOPS_ARG1="${NEKOLOR_R}e"
+else
+  # Committed
+  NEKOPS_ARG1="${NEKOLOR_W}>"
+  NEKOPS_ARG2="${NEKOLOR_W}<"
+fi
+if [[ $git_status =~ [\?][\?][\ ] ]]; then
+  # Untracked
+  NEKOPS_ARG2="${NEKOLOR_B}·"
+fi
+if [ -d ${NEKOPS_HEAD}/.git/rebase-apply ]; then
+  # In Rebase-Apply State
+  NEKOPS_ARG3="${NEKOPS_ARG3} ${NEKOLOR_R}Ra"
+fi
+local stashcnt=$(git stash list|wc -l)
+if [ $stashcnt -gt 0 ]; then
+  # Stashed
+  NEKOPS_ARG3="${NEKOPS_ARG3} ${NEKOLOR_Y}≅ ${stashcnt}"
+fi
 }
 
-gitneko-fresh(){
-  # fresh status
-  if [[ $NEKOPS_T = true ]] && [[ $NEKOPS_HEAD ]]; then
-    gitneko-get
-    PROMPT="${NEKOLOR_W}(${NEKOLOR_G}$(basename $NEKOPS_HEAD)${NEKOLOR_C}${NEKOPS_PATH} ${NEKOLOR_M}%#%b%f%k "
-    if [ -n "${NEKOPS_ARG2}" ]; then
-      NEKOPS="${NEKOLOR_W}(^${NEKOPS_ARG1}${NEKOLOR_W}ω${NEKOPS_ARG2}${NEKOLOR_W}^)~${NEKOPS_ARG3}"
-    else
-      NEKOPS="${NEKOLOR_W}(^${NEKOPS_ARG1}${NEKOLOR_W}ω${NEKOPS_ARG1}${NEKOLOR_W}^)~${NEKOPS_ARG3}"
-    fi
-    RPROMPT="%(?. .${NEKOLOR_R}%?) ${NEKOPS_BRCH}${NEKOPS} ${NEKOLOR_G}<${NEKOLOR_W}%)"
-  else
-    PROMPT=$NEKOPS_SAVL
-    RPROMPT=$NEKOPS_SAVR
-  fi
-  # show python venv prompt
-  if [[ $VIRTUAL_ENV_PROMPT ]] ; then
-    PROMPT=$VIRTUAL_ENV_PROMPT$PROMPT
-  fi
-  # fresh venv save
-  if [[ $VIRTUAL_ENV_PROMPT ]] ; then
-    _OLD_VISUAL_PS1=$NEKOPS_SAV
-  fi
+gitneko-set-lprompt() {
+PROMPT="${NEKOLOR_W}(${NEKOLOR_G}$(basename $NEKOPS_HEAD)${NEKOLOR_C}${NEKOPS_PATH} ${NEKOLOR_M}%#%b%f%k "
+# python venv prompt support
+if [ -v VIRTUAL_ENV ]; then
+  PROMPT=$VIRTUAL_ENV_PROMPT$PROMPT
+fi
 }
 
-gitneko-check() {
-  local basedir=$(pwd)
-  local curdir=$basedir
-  # if HEAD is empty, save current prompt and start searching
-  if [ -z $NEKOPS_HEAD ] ; then
-    if [[ $VIRTUAL_ENV_PROMPT ]] ; then
-      NEKOPS_SAV=$_OLD_VISUAL_PS1
-    else
-      NEKOPS_SAV=$PROMPT
-    fi
-  fi
-  # searching up for .git/HEAD, get relative path to project rootdir
-  until [ ${curdir} -ef / ] ; do
-    if [ -f ${curdir}/.git/HEAD ] ; then
-      # found, set it, fresh PROMPT and return here
-      NEKOPS_PATH=${basedir#$curdir}
-      NEKOPS_HEAD=${curdir}
-      gitneko-fresh
-      return
-    fi
-    curdir=$(dirname ${curdir})
-  done
-  # not found, clear all and recover
-  NEKOPS_HEAD=''
-  NEKOPS_PATH=''
-  NEKOPS_BRCH=''
-  if [[ $VIRTUAL_ENV_PROMPT ]] ; then
-    PROMPT=$VIRTUAL_ENV_PROMPT$NEKOPS_SAV
+gitneko-reset-lprompt() {
+PROMPT=$NEKOPS_SAVL
+# python venv prompt support
+if [ -v VIRTUAL_ENV ]; then
+  PROMPT=$VIRTUAL_ENV_PROMPT$PROMPT
+fi
+}
+
+gitneko-set-rprompt() {
+if [ -n "${NEKOPS_ARG2}" ]; then
+  NEKOPS="${NEKOLOR_W}(^${NEKOPS_ARG1}${NEKOLOR_W}ω${NEKOPS_ARG2}${NEKOLOR_W}^)~${NEKOPS_ARG3}"
+else
+  NEKOPS="${NEKOLOR_W}(^${NEKOPS_ARG1}${NEKOLOR_W}ω${NEKOPS_ARG1}${NEKOLOR_W}^)~${NEKOPS_ARG3}"
+fi
+RPROMPT="%(?. .${NEKOLOR_R}%?) ${NEKOPS_BRCH}${NEKOPS} ${NEKOLOR_G}<${NEKOLOR_W}%)"
+}
+
+gitneko-reset-rprompt() {
+RPROMPT=$NEKOPS_SAVR
+}
+
+gitneko-fresh() {
+if ! $NEKOPS_T; then
+  return
+fi
+local basedir=$(pwd)
+local curdir=$basedir
+# if HEAD is empty, that means prompt has not been set,
+# save current prompt before start searching
+if [ -z $NEKOPS_HEAD ]; then
+  # python venv prompt support
+  if [ -v VIRTUAL_ENV ]; then
+    NEKOPS_SAVL=$_OLD_VIRTUAL_PS1
   else
-    PROMPT=$NEKOPS_SAV
+    NEKOPS_SAVL=$PROMPT
   fi
+  NEKOPS_SAVR=$RPROMPT
+fi
+# searching up for .git/HEAD, get relative path to project rootdir
+until [ ${curdir} -ef / ]; do
+  if [ -f ${curdir}/.git/HEAD ]; then
+    # found, set it, fresh PROMPT and return here
+    NEKOPS_PATH=${basedir#$curdir}
+    NEKOPS_HEAD=${curdir}
+    # set up branch, if ref is a hash, cut its first 7 bits.
+    local refname=$(< ${NEKOPS_HEAD}/.git/HEAD)
+    if [[ $refname =~ "ref: refs/heads/.*" ]]; then
+      refname=${refname#ref: refs/heads/}
+    else
+      refname=${refname:0:6}
+    fi
+    # check if there is a remote branch
+    if [[ -n $(git remote) ]]; then
+      NEKOPS_BRCH="${NEKOLOR_B}${refname} ${NEKOLOR_C}ᛘ"
+    else
+      NEKOPS_BRCH="${NEKOLOR_Y}${refname} ${NEKOLOR_W}ᛘ"
+    fi
+    gitneko-get-status
+    gitneko-set-lprompt
+    gitneko-set-rprompt
+    return
+  fi
+  curdir=$(dirname ${curdir})
+done
+# not found
+NEKOPS_HEAD=''
+NEKOPS_PATH=''
+NEKOPS_BRCH=''
+gitneko-reset-lprompt
+gitneko-reset-rprompt
 }
 
 gitneko-toggle(){
-  if $NEKOPS_T ; then
-    NEKOPS_T=false
-    gitneko-get
-  else
-    NEKOPS_T=true
-  fi
+if [ $NEKOPS_T = true ] ; then
+  NEKOPS_T=false
+  gitneko-fresh
+else
+  NEKOPS_T=true
+fi
 }
 
 function gitneko(){
   case $1 in
-    "-c")
-      gitneko-check
-      ;;
     "-f")
       gitneko-fresh
       ;;
-    "-g")
-      gitneko-get
-      ;;
     "-h")
-      echo "gitneko parameters:"
-      echo "\t -c check prompt"
-      echo "\t -f fresh prompt"
-      echo "\t -g get git status"
-      echo "\t -h show this help"
-      echo "\t -t toggle prompt"
+      print "Hello, I am your git neko! (^@ω@^)"
+      print ""
+      print -P "  eye | git status | X        | Y       "
+      print -P "  ----+------------+----------+---------"
+      print -P "  (^${NEKOLOR_Y}*%b%f%kω| Updated    | [ADU]    | [ADU]   "
+      print -P "  (^${NEKOLOR_G}6%b%f%kω| Staged     | [DMTARC] | [ ]     "
+      print -P "  (^${NEKOLOR_C}0%b%f%kω| Unmerged   | [ MTARC] | [ AMTD] "
+      print -P "  (^${NEKOLOR_M}-%b%f%kω| Ignored    | [!]      | [!]     "
+      print -P "  (^${NEKOLOR_R}e%b%f%kω| Error      | [X]      | [ ]     "
+      print -P "  (^${NEKOLOR_B}·%b%f%kω| Untracked  | [?]      | [?]     "
+      print -P "  (^%b%f%k>%b%f%kω| Commited   | fallback | fallback"
+      print ""
+      print -P "  toy | explanation            "
+      print -P "  ----+------------------------"
+      print -P "  ${NEKOLOR_R}Ra%b%f%k  | In Rebase-Apply process"
+      print -P "  ${NEKOLOR_Y}≅ %b%f%k  | Stashed                "
+      print ""
+      print "gitneko parameters:"
+      print "  -f force prompt fresh"
+      print "  -h show this help"
+      print "  -t toggle prompt"
       ;;
     "-t")
       gitneko-toggle
@@ -174,6 +195,5 @@ function gitneko(){
 }
 
 autoload -Uz add-zsh-hook
-add-zsh-hook chpwd  gitneko-check 
 add-zsh-hook precmd gitneko-fresh
-gitneko-check
+gitneko-fresh
