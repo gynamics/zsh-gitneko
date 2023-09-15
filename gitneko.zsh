51c088e2987382b83989c41484fe93d579a8ebd1
gynamics <dybfysiat@163.com>
2023-09-16 01:03:57 +0800

    gitneko.sh: use git rev-parse instead of manual search change layout: display git upstream at right display git head at left reverse the neko
    add two line mode and cascade mode
    you can toggle them with option -2 and -c.
    
    Signed-off-by: gynamics <dybfysiat@163.com>

--- gitneko.zsh
+++ gitneko.zsh
@@ -1,18 +1,24 @@
 # show a neko indicating repo status in git repos
 # 'git status' may be slow on large projects, you can toggle it off.
 
+if ! [[ -x /usr/bin/git ]]; then
+  echo "\e[31mgit not detected, gitneko will be disabled!\e[0m" >&2
+fi
+
 # save old prompts
-NEKOPS_SAVL=$PROMPT
-NEKOPS_SAVR=$RPROMPT
+NEKOPS_SAVL=''
+NEKOPS_SAVR=''
 # toggle
 NEKOPS_T=true
+NEKOPS_2L=false
+NEKOPS_2C=false
 # gitneko prompt
 NEKOPS_HEAD=''
 NEKOPS_PATH=''
 NEKOPS_BRCH=''
-NEKOPS_ARG1=""
-NEKOPS_ARG2=""
-NEKOPS_ARG3=""
+NEKOPS_ARG1=''
+NEKOPS_ARG2=''
+NEKOPS_ARG3=''
 NEKOLOR_R='%B%F{red}'
 NEKOLOR_G='%B%F{green}'
 NEKOLOR_B='%B%F{blue}'
@@ -22,11 +28,7 @@ NEKOLOR_Y='%B%F{yellow}'
 NEKOLOR_W='%B%F{white}'
 
 # get git status and save it to NEKOPS
-gitneko-get-status() {
-# do not run 'git status' under .git directory
-if [[ $NEKOPS_PATH =~ "\.git" ]]; then
-  return
-fi
+function gitneko-get-status() {
 # reset nekops args
 NEKOPS_ARG1="${NEKOLOR_M}?"
 NEKOPS_ARG2=""
@@ -48,14 +50,14 @@ elif [[ $git_status =~ [X][\ ][\ ] ]]; then
   NEKOPS_ARG1="${NEKOLOR_R}e"
 else
   # Committed
-  NEKOPS_ARG1="${NEKOLOR_W}>"
+  NEKOPS_ARG1="${NEKOLOR_W}<"
 fi
 # set the second argument
 if [[ $git_status =~ [\ ][MTDRC][\ ] ]]; then
   # Y (work tree) Modified
   NEKOPS_ARG2="${NEKOLOR_Y}*"
   # if index clean, then display it to 1
-  if [[ $NEKOPS_ARG1 = "${NEKOLOR_W}>" ]]; then
+  if [[ $NEKOPS_ARG1 = "${NEKOLOR_W}<" ]]; then
     NEKOPS_ARG1=$NEKOPS_ARG2
     NEKOPS_ARG2=""
   fi
@@ -68,9 +70,9 @@ if [ -z $NEKOPS_ARG2 ]; then
   elif [[ $git_status =~ [!][!][\ ] ]]; then
     # Ignored
     NEKOPS_ARG2="${NEKOLOR_M}-"
-  elif [[ $NEKOPS_ARG1 = "${NEKOLOR_W}>" ]]; then
+  elif [[ $NEKOPS_ARG1 = "${NEKOLOR_W}<" ]]; then
     # Clean
-    NEKOPS_ARG2="${NEKOLOR_W}<"
+    NEKOPS_ARG2="${NEKOLOR_W}>"
   else
     NEKOPS_ARG2=$NEKOPS_ARG1
   fi
@@ -88,93 +90,156 @@ if [ $stashcnt -gt 0 ]; then
 fi
 }
 
-gitneko-set-lprompt() {
-PROMPT="${NEKOLOR_W}(${NEKOLOR_G}$(basename $NEKOPS_HEAD)${NEKOLOR_C}${NEKOPS_PATH} ${NEKOLOR_M}%#%b%f%k "
-# python venv prompt support
-if [ -v VIRTUAL_ENV ]; then
-  PROMPT=$VIRTUAL_ENV_PROMPT$PROMPT
-fi
+# reference this function from reddit channel r/zsh 
+# https://www.reddit.com/r/zsh/comments/cgbm24/multiline_prompt_the_missing_ingredient/
+function prompt-length() {
+ emulate -L zsh
+  local -i x y=${#1} m
+  if (( y )); then
+    while (( ${${(%):-$1%$y(l.1.0)}[-1]} )); do
+      x=y
+      (( y *= 2 ))
+    done
+    while (( y > x + 1 )); do
+      (( m = x + (y - x) / 2 ))
+      (( ${${(%):-$1%$m(l.x.y)}[-1]} = m ))
+    done
+  fi
+ echo $x
 }
 
-gitneko-reset-lprompt() {
-PROMPT=$NEKOPS_SAVL
-# python venv prompt support
-if [ -v VIRTUAL_ENV ]; then
-  PROMPT=$VIRTUAL_ENV_PROMPT$PROMPT
-fi
+# reference this function from reddit channel r/zsh 
+# https://www.reddit.com/r/zsh/comments/cgbm24/multiline_prompt_the_missing_ingredient/
+function fill-line() {
+ local left_len=$(prompt-length $1)
+ local right_len=$(prompt-length $2)
+ local pad_len=$((COLUMNS - left_len - right_len - 1))
+ local pad=${(pl.$pad_len.. .)}  # pad_len spaces
+ echo ${1}${pad}${2}
 }
 
-gitneko-set-rprompt() {
-NEKOPS="${NEKOLOR_W}(^${NEKOPS_ARG1}${NEKOLOR_W}ω${NEKOPS_ARG2}${NEKOLOR_W}^)~${NEKOPS_ARG3}"
-RPROMPT="%(?. .${NEKOLOR_R}%?) ${NEKOPS_BRCH}${NEKOPS} ${NEKOLOR_G}<${NEKOLOR_W}%)"
-}
-
-gitneko-reset-rprompt() {
-RPROMPT=$NEKOPS_SAVR
-}
-
-gitneko-fresh() {
-if ! $NEKOPS_T; then
-  return
-fi
-local basedir=$(pwd)
-local curdir=$basedir
-# if HEAD is empty, that means prompt has not been set,
-# save current prompt before start searching
-if [ -z $NEKOPS_HEAD ]; then
+function gitneko-set-prompt() {
+# save prompts
+if [[ -z $NEKOPS_SAVL ]] && [[ -z $NEKOPS_SAVR ]]; then
+  NEKOPS_SAVL=$PROMPT
   # python venv prompt support
   if [ -v VIRTUAL_ENV ]; then
     NEKOPS_SAVL=$_OLD_VIRTUAL_PS1
-  else
-    NEKOPS_SAVL=$PROMPT
   fi
   NEKOPS_SAVR=$RPROMPT
 fi
-# searching up for .git/HEAD, get relative path to project rootdir
-until [ ${curdir} -ef / ]; do
-  if [ -f ${curdir}/.git/HEAD ]; then
-    # found, set it, fresh PROMPT and return here
-    NEKOPS_PATH=${basedir#$curdir}
-    NEKOPS_HEAD=${curdir}
-    # set up branch, if ref is a hash, cut its first 7 bits.
-    local refname=$(< ${NEKOPS_HEAD}/.git/HEAD)
-    if [[ $refname =~ "ref: refs/heads/.*" ]]; then
-      refname=${refname#ref: refs/heads/}
-    else
-      refname=${refname:0:6}
-    fi
-    # check if there is a remote branch
-    if [[ -n $(git remote) ]]; then
-      NEKOPS_BRCH="${NEKOLOR_B}${refname} ${NEKOLOR_C}ᛘ"
-    else
-      NEKOPS_BRCH="${NEKOLOR_Y}${refname} ${NEKOLOR_W}ᛘ"
-    fi
-    gitneko-get-status
-    gitneko-set-lprompt
-    gitneko-set-rprompt
-    return
-  fi
-  curdir=$(dirname ${curdir})
-done
-# not found
-NEKOPS_HEAD=''
-NEKOPS_PATH=''
-NEKOPS_BRCH=''
-gitneko-reset-lprompt
-gitneko-reset-rprompt
-}
 
-gitneko-toggle(){
-if [ $NEKOPS_T = true ] ; then
-  NEKOPS_T=false
-  gitneko-fresh
-else
-  NEKOPS_T=true
+# set prompt
+if [[ -e $NEKOPS_PATH ]]; then
+  local left_bottom="${NEKOLOR_M}%#%b%f%k "
+  local right_bottom="%(?. .${NEKOLOR_R}%?) ${NEKOPS_ARG3} "\
+"${NEKOLOR_W}~(^${NEKOPS_ARG2}"\
+"${NEKOLOR_W}ω${NEKOPS_ARG1}"\
+"${NEKOLOR_W}^)"
+  # set left prompt
+  local left_top="${NEKOLOR_W}(${NEKOLOR_B}${NEKOPS_HEAD} "\
+"${NEKOLOR_C}${PWD#$NEKOPS_PATH}"
+  # set right prompt
+  local right_top="${NEKOLOR_C}ᛘ ${NEKOPS_BRCH} "\
+"${NEKOLOR_G}<${NEKOLOR_W}%)"
+
+  if $NEKOPS_2L ; then
+    PROMPT="$(fill-line ${left_top} ${right_top})"$'\n'
+    RPROMPT="${right_bottom}"
+  else
+    PROMPT="${left_top}"
+    RPROMPT="${right_bottom}${right_top}"
+  fi   
+  # python venv prompt support
+  if [ -v VIRTUAL_ENV ]; then
+    left_bottom=$VIRTUAL_ENV_PROMPT$left_bottom
+  fi
+  PROMPT+="${left_bottom}"
+else # reset
+  PROMPT=$NEKOPS_SAVL
+  # python venv prompt support
+  if [ -v VIRTUAL_ENV ]; then
+    _OLD_VIRTUAL_PS1=$NEKOPS_SAVL
+    PROMPT=$VIRTUAL_ENV_PROMPT$PROMPT
+  fi
+  RPROMPT=$NEKOPS_SAVR
+  # clear save
+  NEKOPS_SAVL=''
+  NEKOPS_SAVR=''
 fi
 }
 
-function gitneko(){
+function gitneko-fresh() {
+NEKOPS_HEAD=''
+NEKOPS_PATH=''
+NEKOPS_BRCH=''
+if ! $NEKOPS_T; then
+  # not enabled
+  gitneko-set-prompt
+  return
+fi
+if $(git rev-parse --is-inside-work-tree 2>/dev/null); then
+  # inside a git worktree
+  NEKOPS_PATH=$(git rev-parse --show-toplevel)
+  # prefer a symbolic ref name
+  NEKOPS_HEAD=$(git branch --show-current)
+  if [ -z $NEKOPS_HEAD ]; then
+    # try to use a commit hash
+    NEKOPS_HEAD=$(git rev-parse --short HEAD 2>/dev/null)
+  fi
+  # is HEAD a symbolic name? (otherwise it is detached)
+  if (git symbolic-ref -q HEAD >/dev/null); then
+    # prefer a symbolic name
+    NEKOPS_BRCH=$(git rev-parse --abbrev-ref --symbolic-full-name @{u} 2>/dev/null)
+    if [ -z $NEKOPS_BRCH ]; then
+      # try to find a commit hash
+      NEKOPS_BRCH=$(git rev-parse --short @{u} 2>/dev/null)
+      if [ -z $NEKOPS_BRCH ]; then
+        # no upstream found
+        NEKOPS_BRCH="${NEKOLOR_R}*"
+      else
+        NEKOPS_BRCH="${NEKOLOR_Y}${NEKOPS_BRCH}"
+      fi
+    else
+      NEKOPS_BRCH="${NEKOLOR_G}${NEKOPS_BRCH}"
+    fi
+  else
+    NEKOPS_BRCH='detached' # detached
+  fi
+  gitneko-get-status
+fi
+# set up prompt
+gitneko-set-prompt
+}
+
+function gitneko-erase() {
+  if $NEKOPS_2C ; then
+    if $NEKOPS_2L ; then
+      print '\e[1A\e[K\e[1A\e[K'
+    else
+      print '\e[1A\e[K'
+    fi
+  fi
+}
+
+function gitneko() {
   case $1 in
+    "-2")
+      if $NEKOPS_2L; then
+        NEKOPS_2L=false
+      else
+        NEKOPS_2L=true
+      fi
+      print "Two-line mode: ${NEKOPS_2L}"
+      ;;
+    "-c")
+      if $NEKOPS_2C; then
+        NEKOPS_2C=false
+      else
+        NEKOPS_2C=true
+      fi
+      print "Cascade mode: ${NEKOPS_2C}"
+      ;;
     "-f")
       gitneko-fresh
       ;;
@@ -201,9 +266,12 @@ function gitneko(){
       print "  -f force prompt fresh"
       print "  -h show this help"
       print "  -t toggle prompt"
+      print "  -2 toggle 2 line mode"
+      print "  -c toggle cascade mode"
       ;;
     "-t")
-      gitneko-toggle
+      NEKOPS_T=!$NEKOPS_T
+      gitneko-fresh
       ;;
     *)
       echo "unknown parameter, -h for help"  
@@ -213,4 +281,5 @@ function gitneko(){
 
 autoload -Uz add-zsh-hook
 add-zsh-hook precmd gitneko-fresh
+add-zsh-hook preexec gitneko-erase
 gitneko-fresh
